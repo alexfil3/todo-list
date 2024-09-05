795461e7c1955ea2969135b003d95186db93050d
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-09-05 18:08:21 +0300

    init

---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const {merge} = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ]
+    }
+})
