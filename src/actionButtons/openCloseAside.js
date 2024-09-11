a583ac5392164eb9fd2582a56202778f32306081
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-09-11 18:22:11 +0300

    add open/close functions to aside

---
+++ src/actionButtons/openCloseAside.js
@@ -0,0 +1,19 @@
+import createOpenAsideBtn from "../helpers/createOpenAsideBtn";
+
+const content = document.querySelector(".content");
+const aside = document.querySelector(".aside");
+
+function openAside() {
+    const openAsideBtn = document.querySelector("#open-aside-btn");
+
+    aside.classList.remove("close-aside");
+    content.removeChild(openAsideBtn);
+}
+
+function closeAside() {
+    aside.classList.add("close-aside");
+    
+    setTimeout(() => createOpenAsideBtn(), 300)
+}
+
+export {openAside, closeAside}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import allTasks from "./pages/allTasks";
-import { closeAside } from "./buttonsFunctions/asideBurger";
+import { closeAside } from "./actionButtons/openCloseAside";
 import "./style.css";
 
-allTasks()
+allTasks();
 
 const closeAsideBtn = document.querySelector("#close-aside-btn");
 
-closeAsideBtn.addEventListener("click", closeAside)
+closeAsideBtn.addEventListener("click", closeAside);
