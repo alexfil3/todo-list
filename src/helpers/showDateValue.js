0c186fc25af480466cb976255874721f8c30a3b6
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-09-13 21:57:13 +0300

    add library to work with dates and complete datePicker

--- src/helpers/showDateValue.js
+++ src/helpers/showDateValue.js
@@ -1,8 +1,32 @@
+import { format, isAfter, isBefore, isToday } from "date-fns";
+
 function showDateValue(e) {
+    const datePicker = document.querySelector(".datepicker-toggle");
     const dateText = document.querySelector(".datepicker-toggle-button p");
     const date = e.target.value;
+    const result = format(date, "PPP");
+    const today = new Date();
     
-    dateText.textContent = date ? date : "Due to";
+    if (isAfter(date, today)) {
+        datePicker.classList.add("green"); 
+        datePicker.classList.remove("red"); 
+        datePicker.classList.remove("yellow"); 
+    }
+
+    if (isBefore(date, today)) {
+        datePicker.classList.add("red"); 
+        datePicker.classList.remove("green"); 
+        datePicker.classList.remove("yellow"); 
+    }
+
+    if (isToday(date)) {
+        datePicker.classList.add("yellow");
+        datePicker.classList.remove("green"); 
+        datePicker.classList.remove("red"); 
+    }
+    
+    
+    dateText.textContent = date ? result : "Due to";
 }
 
 export default showDateValue;
