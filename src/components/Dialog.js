01cfb0f5995cfdc2c47b349b7bd340a2118e5fbd
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-09-16 16:12:52 +0300

    add close method to dialog

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -2,6 +2,16 @@ function Dialog(className) {
     const dialog = document.createElement("dialog");
     dialog.classList.add(className);
 
+    dialog.addEventListener('click', function(event) {
+        const rect = dialog.getBoundingClientRect();
+        const isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
+            rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+        
+        if (!isInDialog) {
+            dialog.close();
+        }
+    });
+
     return dialog;
 }
 
