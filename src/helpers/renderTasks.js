a1f8800cc07e92cf393af956cf68fc7b037e8749
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-09-20 17:43:22 +0300

    add more information about task on the page

--- src/helpers/renderTasks.js
+++ src/helpers/renderTasks.js
@@ -3,7 +3,7 @@ import Button from "../components/Button";
 import Div from "../components/Div";
 import Title from "../components/Title";
 import Paragraph from "../components/Paragraph";
-import { isToday } from "date-fns";
+import { isToday, isAfter, isBefore } from "date-fns";
 
 function renderTasks() {
     const allTasks = JSON.parse(localStorage.getItem("tasks"));
@@ -32,28 +32,75 @@ function renderTasks() {
         list.textContent = "";
     }
 
-        tasksToRender.forEach(task => {
+        tasksToRender.forEach(({title, description, dueDate, priority, project}) => {
             const li = ListItem("tasks-list-item");
             const completedButton = Button("completed-button", "", "completedButton", "../alien-svgrepo-com.svg", "svg");
-            const titleAndDescriptionWrapper = Div("title-and-description-wrapper");
-            const title = Title("h3", task.title, "task-title");
-            const description = Paragraph(task.description, "task-description");
+            const titleDescriptionDueDateWrapper = Div("title-description-due-date-wrapper");
+            const taskTitle = Title("h3", title, "task-title");
+            let taskDescription;
+            if (description) {
+                taskDescription = Paragraph(description, "task-description");
+            }
+            let taskDueDate;
+            if (dueDate) {
+                taskDueDate = Paragraph(dueDate, "task-due-date");
+                let color;
+                const date = new Date(dueDate);
+                const currentDate = new Date();
+                if (isAfter(date, currentDate)) {
+                    color = "green-text";
+                }
+                if (isBefore(date, currentDate)) {
+                    color = "red-text";
+                }
+                if (isToday(date)) {
+                    color = "yellow-text";
+                }
+                taskDueDate.classList.add(color);
+            }
             const manageAndProjectWrapper = Div("manage-and-project-wrapper");
             const editButtonsWrapper = Div("edit-buttons-wrapper");
             const editButton = Button("edit-button", "", "editButton", "../alien-svgrepo-com.svg", "svg");
             const deleteButton = Button("delete-button", "", "deleteButton", "../alien-svgrepo-com.svg", "svg");
-            const project = Paragraph(task.project, "task-project");
+            let taskPriority;
+            if (priority) {
+                taskPriority = Paragraph(priority, "task-priority");
+                let color;
+                switch (priority) {
+                    case "High":
+                        color = "red-text";
+                        break;
+                    case "Medium":
+                        color = "yellow-text";
+                        break;
+                    case "Low":
+                        color = "green-color";
+                        break;
+                    default:
+                        break;
+                }
+                taskPriority.classList.add(color)
+            }
+            const taskProject = Paragraph(project, "task-project");
 
             editButtonsWrapper.appendChild(editButton);
             editButtonsWrapper.appendChild(deleteButton);
             manageAndProjectWrapper.appendChild(editButtonsWrapper);
-            manageAndProjectWrapper.appendChild(project);
+            if (priority) {
+                manageAndProjectWrapper.appendChild(taskPriority);
+            }
+            manageAndProjectWrapper.appendChild(taskProject);
 
-            titleAndDescriptionWrapper.appendChild(title);
-            titleAndDescriptionWrapper.appendChild(description);
+            titleDescriptionDueDateWrapper.appendChild(taskTitle);
+            if (description) {
+                titleDescriptionDueDateWrapper.appendChild(taskDescription);
+            }
+            if (dueDate) {
+                titleDescriptionDueDateWrapper.appendChild(taskDueDate);
+            }
 
             li.appendChild(completedButton);
-            li.appendChild(titleAndDescriptionWrapper);
+            li.appendChild(titleDescriptionDueDateWrapper);
             li.appendChild(manageAndProjectWrapper);
 
             list.appendChild(li);
