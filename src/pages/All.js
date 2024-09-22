65aa808b73b70afc2feca2f691d21cef68739044
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-09-22 18:10:12 +0300

    delete comments in all page

--- src/pages/All.js
+++ src/pages/All.js
@@ -7,24 +7,19 @@ import createAddTaskDialog from "../helpers/createAddTaskDialog";
 import renderTasks from "../helpers/renderTasks";
 
 function All() {
-    // find and clear content before appending child to it
     const main = document.querySelector("#main");
     main.textContent = "";
 
-    // remove active class from other buttons
     const array = Array.from(document.querySelectorAll(".pages-list-button"))
     removeClass(array, "", "active-page-button");
 
-    // add active class to the current button's page
     const activeButton = document.querySelector("#All");
     activeButton.classList.add("active-page-button");
 
-    // call elements that we need on this page
     const title = Title("h2", "All", "main-page-title");
     const button = Button("main-add-task-btn", "Add task", "mainAddTaskButton", "../alien-svgrepo-com.svg", "svg", () => openDialog(createAddTaskDialog));
     const tasksList = List("all-tasks-list", () => console.log("handle all tasks"));
 
-    // append elements
     main.appendChild(title);
     main.appendChild(button);
     main.appendChild(tasksList);
