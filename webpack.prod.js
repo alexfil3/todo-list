ddb831839d849298f6de75feace8ccb131c04c9f
Alex Filimonov <119815135+alexfil3@users.noreply.github.com>
2024-09-27 22:34:45 +0300

    bf

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,4 +4,20 @@ const common = require("./webpack.common");
 module.exports = merge(common, {
     mode: "production",
     devtool: "source-map",
+     module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ]
+    }
 })
